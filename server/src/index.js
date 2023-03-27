e6096787904f5b683db71eee40eb2bfdfd9fd659
Brion <info@brionmario.com>
2023-03-27 18:48:28 +0530

    Comment out `strictQuery`

--- server/src/index.js
+++ server/src/index.js
@@ -44,7 +44,7 @@ app.get('/', (_, res) => {
 });
 
 if (process.env.MONGODB_URI) {
-  mongoose.set('strictQuery', false);
+  // mongoose.set('strictQuery', false);
   mongoose
     .connect(process.env.MONGODB_URI)
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
