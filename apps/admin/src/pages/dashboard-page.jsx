1b103931273235a9564860ec6a64f6a8d55d1940
Brion <info@brionmario.com>
2023-03-28 20:03:08 +0530

    Add dummy dashboard

--- apps/admin/src/pages/dashboard-page.jsx
+++ apps/admin/src/pages/dashboard-page.jsx
@@ -16,8 +16,162 @@
  * under the License.
  */
 
-const DashboardPage = () => {
-  return 'Dashboard';
-};
+import * as React from 'react';
+import {useTheme} from '@mui/material/styles';
+import {LineChart, Line, XAxis, YAxis, Label, ResponsiveContainer} from 'recharts';
+import Link from '@mui/material/Link';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
 
-export default DashboardPage;
+// Generate Sales Data
+function createData(time, amount) {
+  return {time, amount};
+}
+
+const data = [
+  createData('00:00', 0),
+  createData('03:00', 300),
+  createData('06:00', 600),
+  createData('09:00', 800),
+  createData('12:00', 1500),
+  createData('15:00', 2000),
+  createData('18:00', 2400),
+  createData('21:00', 2400),
+  createData('24:00', undefined),
+];
+
+function createOrderData(id, date, name, shipTo, paymentMethod, amount) {
+  return {id, date, name, shipTo, paymentMethod, amount};
+}
+
+const orderRows = [
+  createOrderData(0, '16 Mar, 2019', 'Elvis Presley', 'Tupelo, MS', 'VISA ⠀•••• 3719', 312.44),
+  createOrderData(1, '16 Mar, 2019', 'Paul McCartney', 'London, UK', 'VISA ⠀•••• 2574', 866.99),
+  createOrderData(2, '16 Mar, 2019', 'Tom Scholz', 'Boston, MA', 'MC ⠀•••• 1253', 100.81),
+  createOrderData(3, '16 Mar, 2019', 'Michael Jackson', 'Gary, IN', 'AMEX ⠀•••• 2000', 654.39),
+  createOrderData(4, '15 Mar, 2019', 'Bruce Springsteen', 'Long Branch, NJ', 'VISA ⠀•••• 5919', 212.79),
+];
+
+function Title(props) {
+  return (
+    <Typography component="h2" variant="h6" color="primary" gutterBottom>
+      {props.children}
+    </Typography>
+  );
+}
+
+export default function Chart() {
+  const theme = useTheme();
+
+  return (
+    <React.Fragment>
+      <Container maxWidth="lg" sx={{mt: 4, mb: 4}}>
+        <Grid container spacing={3}>
+          {/* Chart */}
+          <Grid item xs={12} md={8} lg={9}>
+            <Paper
+              sx={{
+                p: 2,
+                display: 'flex',
+                flexDirection: 'column',
+                height: 240,
+              }}
+            >
+              <ResponsiveContainer>
+                <LineChart
+                  data={data}
+                  margin={{
+                    top: 16,
+                    right: 16,
+                    bottom: 0,
+                    left: 24,
+                  }}
+                >
+                  <XAxis dataKey="time" stroke={theme.palette.text.secondary} style={theme.typography.body2} />
+                  <YAxis stroke={theme.palette.text.secondary} style={theme.typography.body2}>
+                    <Label
+                      angle={270}
+                      position="left"
+                      style={{
+                        textAnchor: 'middle',
+                        fill: theme.palette.text.primary,
+                        ...theme.typography.body1,
+                      }}
+                    >
+                      Sales Trends
+                    </Label>
+                  </YAxis>
+                  <Line
+                    isAnimationActive={false}
+                    type="monotone"
+                    dataKey="amount"
+                    stroke={theme.palette.primary.main}
+                    dot={false}
+                  />
+                </LineChart>
+              </ResponsiveContainer>
+            </Paper>
+          </Grid>
+          {/* Recent Deposits */}
+          <Grid item xs={12} md={4} lg={3}>
+            <Paper
+              sx={{
+                p: 2,
+                display: 'flex',
+                flexDirection: 'column',
+                height: 240,
+              }}
+            >
+              <React.Fragment>
+                <Title>Weekly Revenue</Title>
+                <Typography component="p" variant="h4">
+                  $3,024.00
+                </Typography>
+                <Typography color="text.secondary" sx={{flex: 1}}>
+                  on {`${new Date().toLocaleDateString()}`}
+                </Typography>
+              </React.Fragment>
+            </Paper>
+          </Grid>
+          {/* Recent Orders */}
+          <Grid item xs={12}>
+            <Paper sx={{p: 2, display: 'flex', flexDirection: 'column'}}>
+              <React.Fragment>
+                <Title>Recent Orders</Title>
+                <Table size="small">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Date</TableCell>
+                      <TableCell>Name</TableCell>
+                      <TableCell>Ship To</TableCell>
+                      <TableCell>Payment Method</TableCell>
+                      <TableCell align="right">Sale Amount</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {orderRows.map(row => (
+                      <TableRow key={row.id}>
+                        <TableCell>{row.date}</TableCell>
+                        <TableCell>{row.name}</TableCell>
+                        <TableCell>{row.shipTo}</TableCell>
+                        <TableCell>{row.paymentMethod}</TableCell>
+                        <TableCell align="right">{`$${row.amount}`}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </React.Fragment>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
+    </React.Fragment>
+  );
+}
